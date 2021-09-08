822011fee94f95e661dd696aa00c43f626f0e161
mordes89 <mordes89@gmail.com>
2021-09-08 10:39:14 -0400

    css login page

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -39,69 +39,73 @@ class SignupModal extends React.Component {
     return (
       <div className="signup-form">
         <h1 className="signup">Sign Up</h1>
-        <h3 className="qae">It's quick and easy.</h3>
+        <h3 className="quick-and-easy">It's quick and easy.</h3>
         <hr className="hline-signup"/>
         <form onSubmit={this.handleSubmit}> 
-          <h1>{this.renderErrors()}</h1>
-          <div className="name">
-            <label>
-              <input 
-                type="text"
-                value={this.state.first_name}
-                onChange={this.update('first_name')}
-                placeholder="First Name"
-                className="first_name"
-              />
-            </label>
-            <br />
-            <label>
-              <input 
-                type="text"
-                value={this.state.last_name}
-                onChange={this.update('last_name')}
-                placeholder="Last Name"
-                className="last_name"
-              />
-            </label>
-          </div>
-            <br />         
-            <label>
-              <input 
-                type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username"
-              />
-            </label>
-            <br />
-            <label>
-              <input 
-                type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                placeholder="Password"
-              />
-            </label>
-            <br />
-            <label>
-              <input 
-                type="email"
-                value={this.state.email}
-                onChange={this.update('email')}
-                placeholder="email"
-              />
-            </label>
-            <br />
-            <label>
-              <input 
-                type="date"
-                value={this.state.birthday}
-                onChange={this.update('birthday')}
-                placeholder="Birthday"
-              />
-            </label>
-            <br />            
-            <button type="submit">Sign Up</button>
+          {/* <h1>{this.renderErrors()}</h1> */}
+          <div className="centered-signup-form">
+            <div className="name">
+              <label>
+                <input 
+                  type="text"
+                  value={this.state.first_name}
+                  onChange={this.update('first_name')}
+                  placeholder="First Name"
+                  className="first_name"
+                />
+              </label>            
+              <label>
+                <input 
+                  type="text"
+                  value={this.state.last_name}
+                  onChange={this.update('last_name')}
+                  placeholder="Last Name"
+                  className="last_name"
+                />
+              </label>
+            </div>
+              <br />         
+              <label>
+                <input 
+                  type="text"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  placeholder="Username"
+                  className="upe"
+                />
+              </label>
+              <br />
+              <label>
+                <input 
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  placeholder="Password"
+                  className="upe"
+                />
+              </label>
+              <br />
+              <label>
+                <input 
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.update('email')}
+                  placeholder="email"
+                  className="upe"
+                />
+              </label>
+              <br />
+              <label>
+                <input 
+                  type="date"
+                  value={this.state.birthday}
+                  onChange={this.update('birthday')}
+                  placeholder="Birthday"
+                />
+              </label>
+              <br />            
+              <button type="submit" className="signup-button">Sign Up</button>
+            </div>
         </form>
       </div>
     );
